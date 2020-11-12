eb30da6e237b0b757b99289d253d6bb669ac80a5
airson <airson_yu@163.com>
2020-11-12 14:07:51 +0800

    websdk_ui_version: R08.00.37,date: 20201112

--- src/websdk.core.js
+++ src/websdk.core.js
@@ -13,7 +13,7 @@ class websdk {
             login_uid: null,
             login_user: {},
         };
-        logger.debug('websdk_core_version: R08.00.37,date: 20201110');
+        logger.debug('websdk_core_version: R08.00.37,date: 20201112');
 
         //replace_local_ip_for_ie: append activeX node @see processor.js constructor
         window.is_ie = false;
